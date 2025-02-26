98bdaee4412ee22f69e96bb43ab0e50062696b8a
Vikash <phanivikash2@gmail.com>
2025-02-26 23:49:56 +0530

    Initial commit , storing first frame , creating delta frame , creating a threshhold frame

---
+++ main.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy
+import time
+from Mailing import send_email
+
+# Access laptop webcam
+video=cv2.VideoCapture(0)
+time.sleep(1)
+first_frame=None
+
+while True:
+    check, frame = video.read()
+    grey_frame=cv2.cvtColor(frame ,cv2.COLOR_BGR2GRAY)
+    grey_frame_gb=cv2.GaussianBlur(grey_frame,(21,21),0)
+
+
+    if first_frame is None :
+        first_frame = grey_frame_gb
+
+    delta_frame = cv2.absdiff(first_frame,grey_frame_gb)
+
+    thresh_frame=cv2.threshold(delta_frame,50,225,cv2.THRESH_BINARY)[1]
+    dil_frame = cv2.dilate(thresh_frame,None,iterations=2)
+    cv2.imshow("video", dil_frame)
+
+
+
+    cv2.imshow("Updated_video",frame)
+
+
+
+    key=cv2.waitKey(1)
+
+    # Add a key to break the loop and end the video
+    if key==ord('q'):
+        break
+
+
+video.release()
+
+
+
